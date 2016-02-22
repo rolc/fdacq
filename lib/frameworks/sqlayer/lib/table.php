c4ec941de7ea8c68c19d836ea4715545268fa049
ROLCapital <roderic@rolcapital.com>
2016-02-22 16:37:39 -0700

    Create Database Table Class.

---
+++ lib/frameworks/sqlayer/lib/table.php
@@ -0,0 +1,48 @@
+<?php
+
+abstract class SQLayerTable
+{
+    
+    /** @property *str* table name **/
+    protected $tableName;
+    
+    /** @property *obj* SQLayerDbo **/
+    protected $dbo;
+    
+    /** @method get record from key
+      * @param  *int* integer key
+      * @return *arr* assoc (or false) **/
+    public function recFromKey($key)
+    {
+        /** compose sql **/
+        $sql = 'SELECT * FROM "'.$this->tableName.'" WHERE "key" = '.$key.';';
+
+        /** return the first record (or false) **/
+        if ($recs = $this->dbo->fetchRecs($sql)) {
+            foreach ($recs as $rec) {
+                return $rec;
+            }
+        } else {
+            return false;
+        }
+ 
+    }
+
+    /** @method get record from key
+      * @param  *int* integer key
+      * @return *arr* array of assocs (or false) **/
+    public function allRecs()
+    {
+        /** compose sql **/
+        $sql = 'SELECT * FROM "'.$this->tableName.'";';
+
+        /** return the array of recs (or false) **/
+        if ($recs = $this->dbo->fetchRecs($sql)) {
+            return $recs;
+        } else {
+            return false;
+        }
+
+    }
+
+}
